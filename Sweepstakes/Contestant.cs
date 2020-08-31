d3c8cb93af29704ac4e8dc2f59df9b688151e782
NCRadtke00 <raddnc@gmail.com>
2020-08-31 10:57:51 -0500

    Contestant is able to add email, first and last name.

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace Sweepstakes
 {
@@ -12,5 +14,12 @@ namespace Sweepstakes
         public string lastName;
         public string emailAddress;
         public int registrationNumber;
+        public Contestant()
+        {
+            firstName = UserInterface.GetUserInput("Please enter your first name: ");
+            lastName = UserInterface.GetUserInput("Please enter your first name: ");
+            emailAddress = UserInterface.GetUserInput("Please enter your E-mail address: ");
+            //registrationNumber = List<> count++;
+        }
     }
 }
