26a67a90bc06fb3f2ebe437f87db263fe828aea2
NCRadtke00 <raddnc@gmail.com>
2020-08-31 10:45:59 -0500

    created user interface to get input.

---
+++ Sweepstakes/UserInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class UserInterface
+    {
+
+        public static string GetUserInput(string output)
+        {
+            Console.WriteLine(output);
+            string result = Console.ReadLine();
+            return result;
+        }
+    }
+}
