d634b40fe8d0e915583d94a17797a74d44d940ad
NCRadtke00 <raddnc@gmail.com>
2020-08-31 12:01:42 -0500

    Created outlines for Sweepstakes(Queue&stack), sweepstakes, marketingfirm. created interface for isweepstakes.

---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        Dictionary<TKey, TValue> sweepstakes = new Dictionary<TKey, TValue>;
+
+        public Sweepstakes()
+        {
+
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+        public void PickWinner()
+        {
+
+        }
+
+    }
+}
